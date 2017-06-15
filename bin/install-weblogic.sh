dad1ce25bcbfcce49e99eca02949e3c5fd5acaa8
jsam <samuel.picek@camunda.com>
2017-06-15 16:05:57 +0200

    release(12.2v0.1.0-alpha9): Release for 12.2 - test

--- bin/install-weblogic.sh
+++ bin/install-weblogic.sh
@@ -9,7 +9,7 @@ $GCLOUD/gsutil cp gs://camunda-ops/binaries/oracle/weblogic/${WLS_PKG_FILE} /tmp
 mkdir -p ${WLS_TEMP} ${WLS_INSTALL_HOME}
 # do some repairing. something doesn't work with zip and pkzip format
 zip -FFv /tmp/${WLS_PKG_FILE} --out /tmp/${WLS_PKG_FILE}.fixed && unzip -q /tmp/${WLS_PKG_FILE}.fixed -d ${WLS_TEMP}
-java -jar ${WLS_TEMP}/${WLS_INSTALL_FILE} --ignoreSysPrereqs ORACLE_HOME=${WLS_INSTALL_HOME}
+java -jar ${WLS_TEMP}/${WLS_INSTALL_FILE} -ignoreSysPrereqs ORACLE_HOME=${WLS_INSTALL_HOME}
 rm -rf /tmp/${WLS_PKG_FILE}* ${WLS_TEMP}
 
 # Clean caches
